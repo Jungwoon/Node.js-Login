2f13881bedc3d3dab6bf688814b2e0c584a042e6
Jungwoon <jungwoon.park@nielsen.com>
2017-08-11 01:05:17 +0900

    - re design

--- router/login/index.js
+++ router/login/index.js
@@ -24,14 +24,8 @@ router.get('/', function(req, res) {
     var msg;
     var errMsg = req.flash('error');
 
-    console.log("router.get : " + errMsg);
+    if (errMsg) msg = errMsg;
 
-    if (errMsg) {
-        console.log("errMsg : " + errMsg);
-        msg = errMsg;
-    }
-
-    console.log("call login.ejs and message : " + msg);
     res.render('login.ejs', {'message' : msg}); // login.ejs 호출
 });
 
@@ -54,10 +48,6 @@ passport.use('login-local', new LocalStrategy({
             if (err) return done(err);
 
             if (rows.length) {
-                console.log("login email : " + email);
-                console.log("login password : " + password);
-                console.log("login rows[0].password : " + rows[0].password);
-
                 bcrypt.compare(password, rows[0].password, function(err, res) {
                     if (res) {
                         return done(null, { 'email' : email, 'id' : rows[0].uid });
@@ -75,59 +65,40 @@ passport.use('login-local', new LocalStrategy({
 ));
 
 // for Kakao Auth
-// passport.use('login-kakao', new KakaoStrategy({
-//         clientID : '8f110c83b0e6eb21a75785d7129ade26',
-//         callbackURL : '/main'
-//     },
-//     function(accessToken, refreshToken, profile, done){
-//
-//         console.log("profile : " + profile._json);
-//
-//         // 사용자의 정보는 profile에 들어있다.
-//         // User.findOrCreate(..., function(err, user) {
-//         //     if (err) { return done(err); }
-//         //     done(null, user);
-//         // });
-//     }
-// ));
-
+passport.use('login-kakao', new KakaoStrategy({
+        clientID : 'b9a2a6b512bd31fcc10763cb83b83f7f',
+        callbackURL : 'http://localhost:3000/login/oauth/kakao/callback'
+    },
+    function(accessToken, refreshToken, profile, done) {
+        console.log(profile);
+        return done(null, profile);
+    }
+));
 
 router.post('/', function(req, res, next) {
-
-    console.log("login received to login");
-
+    console.log('login local');
     passport.authenticate('login-local', function(err, user, info) {
-
-        console.log("in in in local-login");
-
         if (err) {
-            console.log("inter error");
             res.status(500).json(err); // 500 : Server Error
         }
 
-
         if (!user) {
-            console.log("!user");
             return res.status(401).json(info.message); // 401 : 권한없음
         }
 
         req.logIn(user, function(err) {
-
-            console.log("logIn!!");
-
-            if (err) {
-                console.log("Login. reqIn err");
-                return next(err);
-            }
+            if (err) return next(err);
 
             return res.json(user);
         });
-
-        console.log("done");
-
     }) (req, res, next);
-
 });
 
+
+router.get('/kakao', passport.authenticate('login-kakao'));
+router.get('/oauth/kakao/callback', passport.authenticate('login-kakao', {
+    successRedirect: '/main',
+    failureRedirect: '/'
+}));
 
 module.exports = router;
